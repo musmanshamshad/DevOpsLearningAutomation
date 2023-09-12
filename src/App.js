08fd733eb60c73cc1eb1c407d2492f1d79c001b3
M Usman Shamshad <musmanshamshad@outlook.com>
2023-09-12 10:37:06 +0500

    AUTODEPLOYSET

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js M Usman Shamshad</code> Pipeline.
+          Edit <code>src/App.js M Usman Shamshad</code> Pipeline Newcode.
         </p>
         <a
           className="App-link"
